177239bffae19c36bd81d97398d699beb3cd0908
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2024-06-18 12:14:41 +0200

    #4: Added compose.yml to enable faster reloading of the container

--- Friend-Service/app/service.py
+++ Friend-Service/app/service.py
@@ -1,10 +1,14 @@
-import os
 import paho.mqtt.client as mqtt
 
 with open("mqtt_config.txt", "r") as f:
     lines = f.readlines()
     mqtt_broker = lines[0].split("=")[1].strip()
     mqtt_port = int(lines[1].split("=")[1].strip())
+
+def get_sub_topics(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        return [(line.strip(), 0) for line in lines]
 
 def on_message(client, userdata, msg):
     topic = msg.topic
@@ -15,8 +19,12 @@ def connect_to_mqtt(broker, port):
     print(f"Connecting to MQTT broker: {broker} on port: {port}")
     client = mqtt.Client(callback_api_version = mqtt.CallbackAPIVersion.VERSION2)
     client.on_message = on_message
+    client.on_subscribe = on_subscribe
     client.connect(broker)
-    client.subscribe("GeoGlow/#")
+
+    sub_topics = get_sub_topics("mqtt_sub_topics.txt")
+
+    client.subscribe(sub_topics)
     client.loop_start()
     return client
 
