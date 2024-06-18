8c322e43595731d64b9536f0fe07a617912a14ae
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2024-06-18 12:32:27 +0200

    #4: QoS can now dynamically be set for topics

--- Friend-Service/app/service.py
+++ Friend-Service/app/service.py
@@ -7,40 +7,40 @@ with open("mqtt_config.txt", "r") as f:
 
 def get_sub_topics(filename):
     with open(filename, "r") as f:
-        lines = f.readlines()
-        return [(line.strip(), 0) for line in lines]
+        lines = f.readlines()[1:]
+        lines = [line.strip() for line in lines]
+        topics = [line.split(",")[0] for line in lines]
+        qos = [int(line.split(",")[1]) for line in lines]
+        return list(zip(topics, qos))
 
 def on_message(client, userdata, msg):
     topic = msg.topic
-    payload = msg.payload.decode()
-    print(payload)
-
-def connect_to_mqtt(broker, port):
-    print(f"Connecting to MQTT broker: {broker} on port: {port}")
-    client = mqtt.Client(callback_api_version = mqtt.CallbackAPIVersion.VERSION2)
-    client.on_message = on_message
-    client.on_subscribe = on_subscribe
-    client.connect(broker)
-
-    sub_topics = get_sub_topics("mqtt_sub_topics.txt")
-
-    client.subscribe(sub_topics)
-    client.loop_start()
-    return client
+    print(topic)
 
 def on_connect(client, userdata, flags, reason_code, properties):
-    if flags.session_present:
-        pass
-    if reason_code == 0:
-        print("Successfully established a connection")
-    if reason_code > 0:
-        print("Connection failed")
+    if reason_code.is_failure:
+        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+        sub_topics = get_sub_topics("mqtt_sub_topics.txt")
+        print(sub_topics)
+        client.subscribe(sub_topics)
+
+def on_subscribe(client, userdata, mid, reason_code_list, properties):
+    for reason_code in reason_code_list:
+        if reason_code.is_failure:
+            print(f"Broker rejected you subscription: {reason_code}")
+        else:
+            print(f"Broker granted the following QoS: {reason_code.value}")
 
 def main():
-    client = connect_to_mqtt(mqtt_broker, mqtt_port)
+    print(f"Connecting to MQTT broker: {mqtt_broker} on port: {mqtt_port}")
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_subscribe = on_subscribe
 
-    while True:
-        pass
+    client.connect(mqtt_broker)
+    client.loop_forever()
 
 if __name__ == "__main__":
     main()
